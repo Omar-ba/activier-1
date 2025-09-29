0f6d9cb66c472c8dc3fb1dc4b4869b17afaa5cb9
Omar-ba <omarbenayed2020@gmail.com>
2025-09-29 15:15:57 +0100

    client et connecter

---
+++ src/serverPackage/Server.java
@@ -0,0 +1,12 @@
+package serverPackage;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Je suis un serveur en attente la connexion d'un client ");
+		System.out.println("un client est connecté");
+	}
+
+}
+           
